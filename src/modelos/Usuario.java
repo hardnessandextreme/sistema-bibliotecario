d8e284ca46f5dc5ef5d1d5e4db30984325e1888b
hardnessandextreme <privcuenta371@gmail.com>
2025-10-20 22:53:05 -0500

    añadir atributos y métodos a la clase Usuario, getter para recuperar el string de la fecha y hora de registro

--- src/modelos/Usuario.java
+++ src/modelos/Usuario.java
@@ -1,4 +1,41 @@
 package modelos;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Usuario {
+    private String nombreUsuario;
+    private String apellidoUsuario ;
+    private String cedulaUsuario;
+    private String celularUsuario;
+    private String emailUsuario;
+    private LocalDateTime fechaRegistroUsuario;
+    private boolean estadoUsuario;
+
+    public Usuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+        this.fechaRegistroUsuario = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombreUsuario='" + this.nombreUsuario + '\'' +
+                ", registroUsuario=" + getFechaRegistroUsuario() +
+                '}';
+    }
+
+    public String getFechaRegistroUsuario() {
+        DateTimeFormatter fechaHoraFormato = DateTimeFormatter.ofPattern("dd MMM yyyy, H:mm", new Locale("es", "EC"));
+        return this.fechaRegistroUsuario.format(fechaHoraFormato);
+    }
+}
+
+class prueba{
+    public static void main(String[] args) {
+        Usuario usuario = new Usuario("Jonathan");
+
+        System.out.println(usuario);
+    }
 }
