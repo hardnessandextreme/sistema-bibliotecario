3970322b2b169a7b3bcaffa8d0d1135bccab1f82
hardnessandextreme <privcuenta371@gmail.com>
2025-10-22 22:40:20 -0500

    refactor clase Prestamo: agregar atributos id, usuario, libro, fechaInicio, fechaFin y estado

--- src/modelos/Prestamo.java
+++ src/modelos/Prestamo.java
@@ -1,4 +1,15 @@
 package modelos;
 
+import java.time.LocalDate;
+
 public class Prestamo {
+    private static int contador = 0;
+    private int id;
+    private Usuario usuario;
+    private Libro libro;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private String estado; // activo, devuelto, vencido
+
+    public Prestamo() {}
 }
