eca38065b47f42f9abb9982d1847b1b044e5cdad
hardnessandextreme <privcuenta371@gmail.com>
2025-10-22 22:07:45 -0500

    añadir clase Libro con atributos, constructor y métodos getter y setter

--- src/modelos/Libro.java
+++ src/modelos/Libro.java
@@ -1,4 +1,74 @@
 package modelos;
 
 public class Libro {
+    private static int contadorLibro = 0;
+    private int id;
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private int anioPublicacion;
+    private int copiasDisponibles;
+    private boolean estado;
+
+    public Libro(String titulo, String autor, int anioPublicacion, int copiasDisponibles) {
+        this.id = Libro.contadorLibro++;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anioPublicacion = anioPublicacion;
+        this.copiasDisponibles = copiasDisponibles;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAnioPublicacion() {
+        return this.anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public int getCopiasDisponibles() {
+        return this.copiasDisponibles;
+    }
+
+    public void setCopiasDisponibles(int copiasDisponibles) {
+        this.copiasDisponibles = copiasDisponibles;
+    }
+
+    public boolean isEstado() {
+        return this.estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
 }
+
+
