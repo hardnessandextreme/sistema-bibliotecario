a1dc6fcbfffc467c1254f698a28c056e5a8f25bf
hardnessandextreme <privcuenta371@gmail.com>
2025-10-28 21:49:00 -0500

    agregar atributos id, usuario, motivo, fecha y pagada

--- src/modelos/Multa.java
+++ src/modelos/Multa.java
@@ -1,4 +1,16 @@
 package modelos;
 
+import java.time.LocalDate;
+
 public class Multa {
+    private static int contador = 0;
+    private int id;
+    private Usuario usuario;
+    private String motivo;
+    private LocalDate fecha;
+    private boolean pagada;
+
+    public Multa(Usuario usuario, String motivo, LocalDate fecha, boolean pagada) {
+
+    }
 }
