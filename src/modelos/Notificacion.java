591848c8a9f314952bc9c510433d0ab16114b8aa
hardnessandextreme <privcuenta371@gmail.com>
2025-10-30 21:51:26 -0500

    agregar clase Notificacion con manejo de envío y formato de fecha

--- src/modelos/Notificacion.java
+++ src/modelos/Notificacion.java
@@ -1,4 +1,32 @@
 package modelos;
 
+import java.time.LocalDateTime;
+
 public class Notificacion {
+    private static int contador = 0;
+    private final int id;
+    private Usuario usuario;
+    private String mensaje;
+    private LocalDateTime fechaEnvio;
+
+    public Notificacion(Usuario usuario, String mensaje) {
+        this.id = Notificacion.contador++;
+        this.usuario = usuario;
+        this.mensaje = mensaje;
+        this.fechaEnvio = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return "Notificacion{" +
+                "id=" + id +
+                ", usuario=" + usuario +
+                ", mensaje='" + mensaje + '\'' +
+                ", fechaEnvio=" + fechaEnvio +
+                '}';
+    }
+
+    public void EnviarNotificacion() {
+
+    }
 }
